505e26221e1df7707b1b80cb6d090749a4f319e7
larry.liu <larry.liu@qsf.tw>
2019-04-19 11:24:37 +0800

    [Title] Update [Link] [Symptom] [Root Cause] [Solution]

--- module-framework/src/main/java/com/xiaozi/framework/libs/BaseActivity.java
+++ module-framework/src/main/java/com/xiaozi/framework/libs/BaseActivity.java
@@ -3,17 +3,20 @@ package com.xiaozi.framework.libs;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 
 /**
  * Created by xiaoz on 2017-10-08.
  */
 
-public class BaseActivity extends Activity {
+public class BaseActivity extends AppCompatActivity {
     protected final String LOG_TAG = getClass().getSimpleName();
     protected Handler mHandler = new Handler();
     protected Activity mActivity = this;
     protected DisplayMetrics mDisplayMetrics = null;
+
+    protected static final int REQUEST_PERMISSION_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,10 @@ public class BaseActivity extends Activity {
     }
 
     protected void initView() {
+
+    }
+
+    protected void initView(Bundle savedInstanceState) {
 
     }
 }
